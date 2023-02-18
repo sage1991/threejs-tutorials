a356f4d0be3a8392f2df25d86267d4eb454f973a
Harry <harry.kane@kakaostyle.com>
2023-02-18 23:04:53 +0900

    Add object hierarchy example

---
+++ packages/tutorial-client/src/object-hierarchy.ts
@@ -0,0 +1,123 @@
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import Stats from "three/examples/jsm/libs/stats.module"
+import { GUI } from "dat.gui"
+import {
+  AxesHelper,
+  Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SphereGeometry,
+  Vector3,
+  WebGLRenderer
+} from "three"
+
+const scene = new Scene()
+scene.add(new AxesHelper(5))
+
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.x = 4
+camera.position.y = 4
+camera.position.z = 4
+
+const renderer = new WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(8, 0, 0)
+
+const light1 = new PointLight()
+light1.position.set(10, 10, 10)
+scene.add(light1)
+
+const light2 = new PointLight()
+light2.position.set(-10, 10, 10)
+scene.add(light2)
+
+const object1 = new Mesh(new SphereGeometry(), new MeshPhongMaterial({ color: 0xff0000 }))
+object1.position.set(4, 0, 0)
+scene.add(object1)
+object1.add(new AxesHelper(5))
+
+const object2 = new Mesh(new SphereGeometry(), new MeshPhongMaterial({ color: 0x00ff00 }))
+object2.position.set(4, 0, 0)
+object1.add(object2)
+object2.add(new AxesHelper(5))
+
+const object3 = new Mesh(new SphereGeometry(), new MeshPhongMaterial({ color: 0x0000ff }))
+object3.position.set(4, 0, 0)
+object2.add(object3)
+object3.add(new AxesHelper(5))
+
+window.addEventListener("resize", onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
+}
+
+const gui = new GUI()
+const object1Folder = gui.addFolder("Object1")
+object1Folder.add(object1.position, "x", 0, 10, 0.01).name("X Position")
+object1Folder.add(object1.rotation, "x", 0, Math.PI * 2, 0.01).name("X Rotation")
+object1Folder.add(object1.scale, "x", 0, 2, 0.01).name("X Scale")
+object1Folder.open()
+const object2Folder = gui.addFolder("Object2")
+object2Folder.add(object2.position, "x", 0, 10, 0.01).name("X Position")
+object2Folder.add(object2.rotation, "x", 0, Math.PI * 2, 0.01).name("X Rotation")
+object2Folder.add(object2.scale, "x", 0, 2, 0.01).name("X Scale")
+object2Folder.open()
+const object3Folder = gui.addFolder("Object3")
+object3Folder.add(object3.position, "x", 0, 10, 0.01).name("X Position")
+object3Folder.add(object3.rotation, "x", 0, Math.PI * 2, 0.01).name("X Rotation")
+object3Folder.add(object3.scale, "x", 0, 2, 0.01).name("X Scale")
+object3Folder.open()
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const debug = document.getElementById("debug1") as HTMLDivElement
+
+function animate() {
+  requestAnimationFrame(animate)
+  controls.update()
+  render()
+  const object1WorldPosition = new Vector3()
+  object1.getWorldPosition(object1WorldPosition)
+  const object2WorldPosition = new Vector3()
+  object2.getWorldPosition(object2WorldPosition)
+  const object3WorldPosition = new Vector3()
+  object3.getWorldPosition(object3WorldPosition)
+  debug.innerText =
+    "Red\n" +
+    "Local Pos X : " +
+    object1.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object1WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nGreen\n" +
+    "Local Pos X : " +
+    object2.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object2WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nBlue\n" +
+    "Local Pos X : " +
+    object3.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object3WorldPosition.x.toFixed(2) +
+    "\n"
+  stats.update()
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+animate()
