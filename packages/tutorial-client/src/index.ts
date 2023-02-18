86505c8ad1f04fb8e30c6e15bb22d577710f92d6
Harry <harry.kane@kakaostyle.com>
2023-02-18 22:48:40 +0900

    Add AxesHelper

--- packages/tutorial-client/src/index.ts
+++ packages/tutorial-client/src/index.ts
@@ -5,7 +5,9 @@ import {
   BoxGeometry,
   MeshBasicMaterial,
   Mesh,
-  Color
+  Color,
+  AxesHelper,
+  Vector3
 } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import Stats from "three/examples/jsm/libs/stats.module"
@@ -13,9 +15,13 @@ import { GUI } from "dat.gui"
 
 const scene = new Scene()
 scene.background = new Color(0xefefef)
+scene.add(new AxesHelper(5))
 
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.x = 2
+camera.position.y = 2
 camera.position.z = 2
+camera.lookAt(new Vector3(10, 10, 20))
 
 const renderer = new WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -47,12 +53,30 @@ document.body.appendChild(stats.dom)
 
 const gui = new GUI()
 const cubeFolder = gui.addFolder("Cube")
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
+cubeFolder.add(cube, "visible")
+cubeFolder.open()
+
+const cubeRotationFolder = cubeFolder.addFolder("Rotation")
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2)
+cubeRotationFolder.open()
+
+const cubePositionFolder = cubeFolder.addFolder("Position")
+cubePositionFolder.add(cube.position, "x", -10, 10)
+cubePositionFolder.add(cube.position, "y", -10, 10)
+cubePositionFolder.add(cube.position, "z", -10, 10)
+cubePositionFolder.open()
+
+const cubeScaleFolder = cubeFolder.addFolder("Scale")
+cubeScaleFolder.add(cube.scale, "x", 0, 5)
+cubeScaleFolder.add(cube.scale, "y", 0, 5)
+cubeScaleFolder.add(cube.scale, "z", 0, 5)
+cubeScaleFolder.open()
 
 const cameraFolder = gui.addFolder("Camera")
 cameraFolder.add(camera.position, "z", 0, 20)
+cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
