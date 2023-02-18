107489aec457592c88893ef80c24c85e7de26213
Harry <harry.kane@kakaostyle.com>
2023-02-18 22:18:38 +0900

    Configure stats and Dat.gui

--- packages/tutorial-client/src/index.ts
+++ packages/tutorial-client/src/index.ts
@@ -4,10 +4,15 @@ import {
   WebGLRenderer,
   BoxGeometry,
   MeshBasicMaterial,
-  Mesh
+  Mesh,
+  Color
 } from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import Stats from "three/examples/jsm/libs/stats.module"
+import { GUI } from "dat.gui"
 
 const scene = new Scene()
+scene.background = new Color(0xefefef)
 
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
@@ -15,6 +20,10 @@ camera.position.z = 2
 const renderer = new WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.addEventListener("change", render)
 
 const geometry = new BoxGeometry()
 const material = new MeshBasicMaterial({
@@ -26,7 +35,6 @@ const cube = new Mesh(geometry, material)
 scene.add(cube)
 
 window.addEventListener("resize", onWindowResize, false)
-
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
@@ -34,13 +42,26 @@ function onWindowResize() {
   render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder("Cube")
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
+
+const cameraFolder = gui.addFolder("Camera")
+cameraFolder.add(camera.position, "z", 0, 20)
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
 
   render()
+  stats.update()
 }
 
 function render() {
